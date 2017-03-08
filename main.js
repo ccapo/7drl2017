51ce71c23357819efabdf2243b37d7d029111613
Christopher Capobianco <ccapo@users.noreply.github.com>
2017-03-07 23:04:28 -0500

    Bumped electron version, increased height, moved menu creation to createWindow function and added dock menu for MacOS

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
+
+app.setName('EscapeCraft')
+
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
@@ -14,9 +17,15 @@ const url = require('url');
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
+function newGameCallback(response) {
+  if (response === 0) {
+    mainWindow.webContents.send('new-game', 1);
+  }
+}
+
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1068, height: 830});
+  mainWindow = new BrowserWindow({width: 1068, height: 850});
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -39,7 +48,218 @@ function createWindow() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
-  })
+  });
+
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and require them here.
+  const template = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'New Game',
+          accelerator: 'CmdOrCtrl+N',
+          click() {
+            const options = {
+              type: 'info',
+              title: 'New Game',
+              message: 'Would you like to start a New Game?',
+              buttons: ['OK', 'Cancel']
+            };
+            dialog.showMessageBox(options, newGameCallback);
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'quit'
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          role: 'undo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'cut'
+        },
+        {
+          role: 'copy'
+        },
+        {
+          role: 'paste'
+        }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          role: 'reload'
+        },
+        {
+          role: 'toggledevtools'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'resetzoom'
+        },
+        {
+          role: 'zoomin'
+        },
+        {
+          role: 'zoomout'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'togglefullscreen'
+        }
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'About',
+          click() {
+            const options = {
+              type: 'info',
+              title: 'About EscapeCraft',
+              message: 'The goal is to survive and escape by crafting weapons/tools\n\nCreated for the 2017 7DRL Challenge\n\nWritten by Chris Capobianco',
+              buttons: ['OK']
+            };
+            dialog.showMessageBox(options);
+          }
+        },
+        {
+          label: 'Controls',
+          click() {
+            const options = {
+              type: 'info',
+              title: 'Controls',
+              message: 'The player can move using either the arrow/WASD keys\n\nThe Enter key is the action key\n\nInteract with the inventory using the mouse',
+              buttons: ['OK']
+            };
+            dialog.showMessageBox(options);
+          }
+        }
+      ]
+    }
+  ];
+
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        {
+          role: 'about',
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'services',
+          submenu: [
+            {
+              label: 'New Game',
+              accelerator: 'CmdOrCtrl+N',
+              click() {
+                const options = {
+                  type: 'info',
+                  title: 'New Game',
+                  message: 'Would you like to start a New Game?',
+                  buttons: ['OK', 'Cancel']
+                };
+                dialog.showMessageBox(options, newGameCallback);
+              }
+            }
+          ]
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'hide'
+        },
+        {
+          role: 'hideothers'
+        },
+        {
+          role: 'unhide'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'quit'
+        }
+      ]
+    });
+
+    // Edit menu.
+    template[1].submenu.push(
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Speech',
+        submenu: [
+          {
+            role: 'startspeaking'
+          },
+          {
+            role: 'stopspeaking'
+          }
+        ]
+      }
+    );
+
+    // Window menu.
+    template[3].submenu = [
+      {
+        label: 'Close',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+      },
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+      },
+      {
+        label: 'Zoom',
+        role: 'zoom'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Bring All to Front',
+        role: 'front'
+      }
+    ];
+
+    const dockMenu = Menu.buildFromTemplate([{
+      label: 'New Game',
+      click() {
+        newGameCallback(0);
+      }
+    }]);
+    app.dock.setMenu(dockMenu)
+  }
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 }
 
 // This method will be called when Electron has finished
@@ -63,212 +283,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-function newGameCallback(response) {
-  if (response === 0) {
-    mainWindow.webContents.send('new-game', 1);
-  }
-}
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-const template = [
-  {
-    label: 'File',
-    submenu: [
-      {
-        label: 'New Game',
-        accelerator: 'CmdOrCtrl+N',
-        click() {
-          const options = {
-            type: 'info',
-            title: 'New Game',
-            message: 'Would you like to start a New Game?',
-            buttons: ['OK', 'Cancel']
-          };
-          dialog.showMessageBox(options, newGameCallback);
-        }
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'quit'
-      }
-    ]
-  },
-  {
-    label: 'Edit',
-    submenu: [
-      {
-        role: 'undo'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'cut'
-      },
-      {
-        role: 'copy'
-      },
-      {
-        role: 'paste'
-      }
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {
-        role: 'reload'
-      },
-      {
-        role: 'toggledevtools'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'resetzoom'
-      },
-      {
-        role: 'zoomin'
-      },
-      {
-        role: 'zoomout'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'togglefullscreen'
-      }
-    ]
-  },
-  {
-    role: 'help',
-    submenu: [
-      {
-        label: 'About',
-        click() {
-          const options = {
-            type: 'info',
-            title: 'About EscapeCraft',
-            message: 'The goal is to survive and escape by crafting weapons/tools\n\nCreated for the 2017 7DRL Challenge\n\nWritten by Chris Capobianco',
-            buttons: ['OK']
-          };
-          dialog.showMessageBox(options);
-        }
-      },
-      {
-        label: 'Controls',
-        click() {
-          const options = {
-            type: 'info',
-            title: 'Controls',
-            message: 'The player can move using either the arrow/WASD keys\n\nThe Enter key is the action key\n\nInteract with the inventory using the mouse',
-            buttons: ['OK']
-          };
-          dialog.showMessageBox(options);
-        }
-      }
-    ]
-  }
-];
-
-if (process.platform === 'darwin') {
-  template.unshift({
-    label: app.getName(),
-    submenu: [
-      {
-        role: 'about',
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'services',
-        submenu: [
-          {
-            label: 'New Game',
-            accelerator: 'CmdOrCtrl+N',
-            click() {
-              const options = {
-                type: 'info',
-                title: 'New Game',
-                message: 'Would you like to start a New Game?',
-                buttons: ['OK', 'Cancel']
-              };
-              dialog.showMessageBox(options, newGameCallback);
-            }
-          }
-        ]
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'hide'
-      },
-      {
-        role: 'hideothers'
-      },
-      {
-        role: 'unhide'
-      },
-      {
-        type: 'separator'
-      },
-      {
-        role: 'quit'
-      }
-    ]
-  });
-
-  // Edit menu.
-  template[1].submenu.push(
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Speech',
-      submenu: [
-        {
-          role: 'startspeaking'
-        },
-        {
-          role: 'stopspeaking'
-        }
-      ]
-    }
-  );
-
-  // Window menu.
-  template[3].submenu = [
-    {
-      label: 'Close',
-      accelerator: 'CmdOrCtrl+W',
-      role: 'close'
-    },
-    {
-      label: 'Minimize',
-      accelerator: 'CmdOrCtrl+M',
-      role: 'minimize'
-    },
-    {
-      label: 'Zoom',
-      role: 'zoom'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Bring All to Front',
-      role: 'front'
-    }
-  ];
-}
-
-const menu = Menu.buildFromTemplate(template);
-Menu.setApplicationMenu(menu);
